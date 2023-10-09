626b77a3e48e8e6837fad39804f7176cbd31557e
Bintou <fofana_bintou@outlook.fr>
2023-10-09 21:47:13 +0200

    Implementation de la page d'affichage des employes

---
+++ src/components/EmployeeListPage.js
@@ -0,0 +1,28 @@
+import DatasTable from "./DatasTable.js"
+import { useSelector } from 'react-redux';
+
+let header = [
+    {key:"firstName",value:"Firstname"},
+    {key:"lastName",value:"LastName"},
+    {key:"birthDate",value:"Birth Date"},
+    {key:"startDate",value:"Start Date"},
+    {key:"street",value:"Street"},
+    {key:"city",value:"City"},
+    {key:"state",value:"State"},
+    {key:"zipCode",value:"Zip Code"},
+    {key:"department",value:"Department"}];
+
+
+function EmployeeListPage(){
+    
+    let employees = useSelector(state => state.employees);
+    return (
+        <div class="employees-list_container">
+            <h1>Current Employees</h1>
+            
+            <DatasTable head={header} datas={employees}></DatasTable>
+        </div>
+    )
+}
+
+export default EmployeeListPage;
